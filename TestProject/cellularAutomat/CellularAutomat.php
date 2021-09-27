5dacaddf4af0c627503813a7914c21129f760aa1
Di <dj_2463@web.de>
2021-09-27 12:43:23 +0200

    Inital first version of GoL.

---
+++ TestProject/cellularAutomat/CellularAutomat.php
@@ -0,0 +1,112 @@
+<?php
+
+class CellularAutomat
+{
+    public $heigth;
+    public $width;
+    public $board = [0, 1, 2, 3, 4, 5, [0, 1, 2, 3, 4, 5]];
+
+    /**
+     * @param $_heigth
+     * @param $_width
+     */
+    function __construct($_heigth, $_width)
+    {
+        $this->heigth=$_heigth;
+        $this->width=$_width;
+    }
+
+    /**
+     * Creation of the board which consists of cells and set as rows and cols
+     */
+    function createBoard(){
+        for($y = 0; $y < $this->width; $y++) {
+            $row = [];
+            for($x = 0; $x < $this->heigth; $x++) {
+                $row[$x] = rand(0,1);
+            }
+        }
+        $this->board[$y] = $row;
+    }
+
+    /**
+     * @param $x
+     * @param $y
+     * @return int
+     *
+     * The living and dead neighbors will be counted around the base cell
+     */
+    function countLivingNeighbors($x, $y)
+    {
+        $coordinates = [
+            [-1, -1], [-1, 0], [-1, 1],
+            [0, -1], [0, 1],
+            [1, -1], [1, 0], [1, 1]
+        ];
+        $count = 0;
+
+
+        foreach($coordinates as $coordinate)
+        {
+            if(isset($this->board[$x + $coordinate[0]][$y + $coordinate[1]])
+                && $this->board[$x + $coordinate[0]][$y + $coordinate[1]] == 1)
+            {
+                $count++;
+            }
+        }
+        return $count;
+    }
+
+    /**
+     * On base of the living and dead neighbors, the next generation will be created
+     */
+    function calculateNextGeneration() {
+        $newBoard= [];
+
+        foreach($this->board as $widthID => $width)
+        {
+            $newBoard[$widthID] = [];
+            foreach ($width as $heightID => $height)
+            {
+                $count = $this->countLivingNeighbors($widthID, $heightID);
+
+                $newValue=null;
+                if($height == 1)
+                {
+                    if($count < 2 || $count > 3)
+                    {
+                        $newValue=0;
+                    } else
+                    {
+                        $newValue=1;
+                    }
+                } else
+                {
+                    if($count == 3)
+                    {
+                        $newValue = 1;
+                    }
+                }
+
+                $newBoard[$widthID][$heightID] = $newValue;
+            }
+        }
+    }
+
+    /**
+     *
+     * Creation of a glider which runs/glids thorugh the board
+     */
+    function createGlider()
+    {
+        for($y = 0; $y < $this->width; $y++) {
+            $row = [];
+            for($x = 0; $x < $this->heigth; $x++) {
+                $row[$x] = rand(0,1);
+            }
+        }
+        $this->board[$y] = $row;
+
+
+    }
+}
