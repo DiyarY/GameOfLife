74c2b2c308e1ec79b5403ff109bdda9fc112a373
Di <dj_2463@web.de>
2021-09-27 15:33:11 +0200

    Create testGOF

--- TestProject/testGOF.php
+++ TestProject/testGOF.php
@@ -1,4 +1,15 @@
 <?php
 
-require_once "CellularAutomat.php";
+require_once "cellularAutomat\CellularAutomat.php";
 
+$sizeRangeOfBoard = 100;
+
+$createCellularAutomat = new CellularAutomat(5, 5);
+$createCellularAutomat->createBoard();
+
+for($i = 0; $i < $sizeRangeOfBoard; $i++)
+{
+    echo "Generation: ".$i."\n";
+    $createCellularAutomat->printOutBoard();
+    $createCellularAutomat->calculateNextGeneration();
+}
