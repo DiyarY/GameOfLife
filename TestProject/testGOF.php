8a944696a5f848906db122f4f45dca43eb900f7e
Di <dj_2463@web.de>
2021-09-30 13:46:28 +0200

    Add documentation

--- TestProject/testGOF.php
+++ TestProject/testGOF.php
@@ -4,11 +4,13 @@ require_once "CellularAutomat.php";
 /**
  * Total number of rows and cols
  */
-$createCellularAutomat = new CellularAutomat(8, 8);
+$createCellularAutomat = new CellularAutomat(10, 10);
 $createCellularAutomat->createBoard();
 
-for($i = 0; $i < 5; $i++)
+//Configure the generations
+for($i = 0; $i < 2; $i++)
 {
+    //Print out the generations
     echo "\nVersion: ".$i."\n";
     $createCellularAutomat->printOutBoard();
     $createCellularAutomat->calculateNextGeneration();
